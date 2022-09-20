9367aa75a1c9ee9656ede0a43e8ccf73f52e3506
rainysteven1 <rainysteven1@gmail.com>
2022-09-20 16:33:15 +0800

    项目初始化

---
+++ config/routes.ts
@@ -0,0 +1,65 @@
+﻿/*
+ * @Author: Rainy
+ * @Email: rainysteven1@gmail.com
+ * @Date: 2022-09-19 21:54:44
+ * @FilePath: /visualization-fronted/config/routes.ts
+ * @Description: 路由
+ */
+export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        name: 'login',
+        path: '/user/login',
+        component: './user/Login',
+      },
+      {
+        name: 'register',
+        path: '/user/register',
+        component: './user/Register'
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
